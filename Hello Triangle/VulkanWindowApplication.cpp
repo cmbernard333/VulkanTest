901bcb5779e0512cece357c56b5ac5d350f1d8d4
Christian Bernard <cmbernard333@gmail.com>
2017-09-19 13:20:23 -0600

    added code for setting up and tearing down vkDebugReportCallbackEXT. Two proxy functions were created to load the address of the create and destroy functions for the debug callback because extensions are not loaded at the time they are needed. the proxy functions are prototyped in the header and implemented in the obj file. The VDebugReportCallback instance variable is only initialized if validation layers are being used.

--- Hello Triangle/VulkanWindowApplication.cpp
+++ Hello Triangle/VulkanWindowApplication.cpp
@@ -5,6 +5,32 @@
 #include <iostream>
 #include <stdexcept>
 #include <functional>
+#include <cstring>
+
+/* proxy functions */
+
+VkResult CreateDebugReportCallbackEXT(VkInstance Instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo,
+	const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback)
+{
+	auto func = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(Instance, VK_CREATE_DEBUG_REPORT_CALLBACK_EXT_FNAME);
+	if (func != nullptr)
+	{
+		return func(Instance, pCreateInfo, pAllocator, pCallback);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void DestroyDebugReportCallbackEXT(VkInstance Instance, VkDebugReportCallbackEXT Callback, const VkAllocationCallbacks* pAllocator)
+{
+	auto func = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(Instance, VK_DESTROY_DEBUG_REPORT_CALLBACK_EXT_FNAME);
+	if (func != nullptr)
+	{
+		func(Instance, Callback, pAllocator);
+	}
+}
 
 /* public */
 void VulkanWindowApplication::Run()
@@ -28,15 +54,76 @@ void VulkanWindowApplication::InitWindow()
 void VulkanWindowApplication::InitVulkan()
 {
 	this->CreateVulkanInstance();
+	this->SetupDebugCallback();
+}
+bool VulkanWindowApplication::CheckValidationLayerSupport(const char* const* LayerNames, std::size_t LayerNamesCount)
+{
+	uint32_t LayerCount;
+	std::vector<VkLayerProperties> AvailableLayers;
+	const char* LayerName = nullptr;
+
+	vkEnumerateInstanceLayerProperties(&LayerCount, nullptr);
+	AvailableLayers.resize(LayerCount);
+	vkEnumerateInstanceLayerProperties(&LayerCount, AvailableLayers.data());
+
+	// check if all the layers in passed in LayerNames are available on this platform
+	for (std::size_t i = 0; i < LayerNamesCount; i++)
+	{
+		LayerName = LayerNames[i];
+		bool bLayerFound = false;
+		for (const auto& LayerProperties : AvailableLayers)
+		{
+			if (strcmp(LayerName, LayerProperties.layerName) == 0)
+			{
+				bLayerFound = true;
+				break;
+			}
+		}
+
+		if (!bLayerFound)
+		{
+			return false;
+		}
+	}
+
+	std::cout << "All Validation Layers Supported." << std::endl;
+
+	return true;
+}
+void VulkanWindowApplication::SetupDebugCallback()
+{
+	VkDebugReportCallbackCreateInfoEXT CallbackCreateInfo = {};
+	if (!gbEnableValidationLayers)
+	{
+		return;
+	}
+	CallbackCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	// filters the types of messages you would like to receive
+	CallbackCreateInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+	CallbackCreateInfo.pfnCallback = VulkanWindowApplication::DebugCallback;
+	// optional : set the userData field to a pointer to the class itself
+	// CallbackCreateInfo.userData = this;
+	
+	// use the proxy function to create a debug callback given the VkDebugReportCallbackCreateInfoEXT struct
+	if (VK_SUCCESS != CreateDebugReportCallbackEXT(this->VInstance, &CallbackCreateInfo, nullptr, &this->VDebugReportCallback))
+	{
+		throw std::runtime_error("Failed to set up debug callback!");
+	}
+
+
 }
 void VulkanWindowApplication::CreateVulkanInstance()
 {
-	uint32_t glfwExtensionCount = 0;
-	const char** glfwExtensions = nullptr;
+	std::vector<const char*> glfwExtensions;
 
 	VkApplicationInfo AppInfo = {};
 	VkInstanceCreateInfo CreateInfo = {};
 	VkResult result;
+
+	if (gbEnableValidationLayers && !CheckValidationLayerSupport())
+	{
+		throw std::runtime_error("Validation layers requested, but not available!");
+	}
 
 	// application information
 	AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -47,14 +134,28 @@ void VulkanWindowApplication::CreateVulkanInstance()
 	AppInfo.apiVersion = VK_API_VERSION_1_0;
 
 	// retrieve the required extensions to interface with window system
-	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	glfwExtensions = this->GetRequiredExtensions(gbEnableValidationLayers);
 
 	// instance creation information
 	CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	CreateInfo.pApplicationInfo = &AppInfo;
-	CreateInfo.enabledExtensionCount = glfwExtensionCount;
-	CreateInfo.ppEnabledExtensionNames = glfwExtensions;
-	CreateInfo.enabledLayerCount = 0; // TODO: global validation layer enablement
+
+	// extensions
+	CreateInfo.enabledExtensionCount = static_cast<uint32_t>(glfwExtensions.size());
+	CreateInfo.ppEnabledExtensionNames = glfwExtensions.data();
+
+	// validation layers
+	if (gbEnableValidationLayers)
+	{
+		// TODO: this is assuming these are in use - this should really be configurable
+		CreateInfo.enabledLayerCount = static_cast<uint32_t>(ValidationLayers.size());
+		CreateInfo.ppEnabledLayerNames = ValidationLayers.data();
+	}
+	else
+	{
+		CreateInfo.enabledLayerCount = 0;
+	}
+		
 
 	// TODO: for fun and enjoyment print the list of extensions supported
 	// TODO: compare the list of extensions returned by `glfwGetRequiredInstanceExtensions` and GetSupporteExtensions()
@@ -85,13 +186,34 @@ void VulkanWindowApplication::Cleanup()
 {
 	// TODO: use RAII instead (destructor)
 	// TOOD: order matters here
+	DestroyDebugReportCallbackEXT(this->VInstance, this->VDebugReportCallback, nullptr);
 	glfwDestroyWindow(this->GlfwWindow);
-
 	vkDestroyInstance(this->VInstance, nullptr);
-
 	glfwTerminate();
 }
 
+// Note: C++ 11 std::vector<T> has std::move<T> semantics
+std::vector<const char*> VulkanWindowApplication::GetRequiredExtensions(bool bEnableValidationLayers)
+{
+	std::vector<const char*> Extensions;
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = nullptr;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	for (uint32_t i = 0; i < glfwExtensionCount; i++)
+	{
+		Extensions.push_back(glfwExtensions[i]);
+	}
+
+	// if we have layers enabled add the "VK_EXT_debug_report" extension
+	if (bEnableValidationLayers)
+	{
+		Extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+	}
+
+	return Extensions;
+}
 
 // Note: C++ 11 std::vector<T> has std::move<T> semantics
 std::vector<VkExtensionProperties> VulkanWindowApplication::GetSupportedExtensions()
@@ -106,4 +228,10 @@ std::vector<VkExtensionProperties> VulkanWindowApplication::GetSupportedExtensio
 	vkEnumerateInstanceExtensionProperties(nullptr, &ExtensionCount, SupportedExtensions.data());
 	std::cout << "Enumerated " << SupportedExtensions.size() << " extensions." << std::endl;
 	return SupportedExtensions;
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL VulkanWindowApplication::DebugCallback(VkDebugReportFlagsEXT Flags, VkDebugReportObjectTypeEXT ObjType, uint64_t Obj, size_t Location, int32_t Code, const char * LayerPrefix, const char * Msg, void * UserData)
+{
+	std::cerr << "validation layer: " << Msg << std::endl;
+	return VK_FALSE;
 }
--- Hello Triangle/VulkanWindowApplication.hpp
+++ Hello Triangle/VulkanWindowApplication.hpp
@@ -2,9 +2,33 @@
 #define GLFW_INCLUDE_VULKAN // force GLFW to include vulkan header
 #include <GLFW/glfw3.h>
 #include <vector>
+#include <array>
+#include <cstdlib>
 
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
+#ifndef NDEBUG
+constexpr bool gbEnableValidationLayers = true;
+#else
+constexpr bool gbEnableValidationLayers = false;
+#endif
+
+// static array of the validation layers to use if support is enabled
+constexpr std::array<const char*,1> ValidationLayers {
+	"VK_LAYER_LUNARG_standard_validation"
+};
+
+constexpr char VK_CREATE_DEBUG_REPORT_CALLBACK_EXT_FNAME[] = "vkCreateDebugReportCallbackEXT";
+constexpr char VK_DESTROY_DEBUG_REPORT_CALLBACK_EXT_FNAME[] = "vkDestroyDebugReportCallbackEXT";
+
+// TODO: move proxy functions to a separate header
+// proxy function to load the pointer address of "vkCreateDebugReportCallbackEXT" because
+// it is an extension function that requires loading
+VkResult CreateDebugReportCallbackEXT(VkInstance Instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo,
+	const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback);
+
+// proxy function to destroy the pointer created as a part of "CreateDebugReportCallbackEXT"
+void DestroyDebugReportCallbackEXT(VkInstance Instance, VkDebugReportCallbackEXT Callback, const VkAllocationCallbacks* pAllocator);
 
 class VulkanWindowApplication {
 public:
@@ -18,6 +42,14 @@ private:
 	 * Initialize the Vulkan instance and associated resources
 	 */
 	void InitVulkan();
+	/**
+	 * Checks if all the requested layers are available
+	 */
+	bool CheckValidationLayerSupport(const char* const* LayerNames = ValidationLayers.data(), std::size_t LayerNamesCount = ValidationLayers.size());
+	/*
+	* Setups the debug callback(s) if validation layers are enabled for Vulkan
+	*/
+	void SetupDebugCallback();
 	/*
 	 * Create the Vulkan instance
 	 */
@@ -33,10 +65,46 @@ private:
 	 */
 	void Cleanup();
 
+	/*
+	 * Retrieves the required extensions for glfw to interact with this platform
+	 * @Param bEnableValidationLayers - if true the validation layer extension is added
+	 * @Return - vector containing the required extensions for this platform
+	 */
+	std::vector<const char*> GetRequiredExtensions(bool bEnableValidationLayers);
+
+	/*
+	 * Retrieves all the supported vulkan extensions for this platform
+	 * @Return vector of all the supported extensions
+	 */
 	std::vector<VkExtensionProperties> GetSupportedExtensions();
+
+
+	/*
+	* Static Debug Callback for reporting extension names using the validation layer API
+	* @Param Flags = the type of callback message, bitmask
+	* @Param ObjType = specifies the type of object that is the subject of the message
+	* @Param obj = the actual vulkan object
+	* @Param location = 
+	* @Param code = 
+	* @Param LayerPrefix = 
+	* @Param Msg = the debug message itself
+	* @Param UserData = user data passed to the callback
+	* @Return - a boolean that indicates if the Vulkan call that triggered the validation layer should be aborted
+	*/
+	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+		VkDebugReportFlagsEXT Flags,
+		VkDebugReportObjectTypeEXT ObjType,
+		uint64_t Obj,
+		size_t Location,
+		int32_t Code,
+		const char* LayerPrefix,
+		const char* Msg,
+		void* UserData);
 
 	// Window pointer
 	GLFWwindow* GlfwWindow;
 	// Vulkan Instance
 	VkInstance VInstance;
+	// the callback handle
+	VkDebugReportCallbackEXT VDebugReportCallback;
 };
